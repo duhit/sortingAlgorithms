a90666cc7de7df006ef681117921374f966dff51
Duhit Choudhary <Duhit Choudhary>
2016-03-14 12:38:22 +0000

    added bubble sort for integer and for random number

--- BubbleSortRandom.java
+++ BubbleSortRandom.java
@@ -6,10 +6,10 @@ import java.util.Random;
 public class BubbleSortRandom {
 	
 	public static int[] generateRandomIntList(){
-		int[] integerList = new int[10000];
+		int[] integerList = new int[100000];
 		Random randomInt = new Random();
 		int n = 0;
-		while(n < 10000){
+		while(n < 100000){
 			integerList[n] = randomInt.nextInt(Integer.MAX_VALUE);
 			n++;
 		}
@@ -22,6 +22,7 @@ public class BubbleSortRandom {
 		int n = list.length;
 		int temp = 0;
 		long sms = System.currentTimeMillis();
+		long count = 0;
 		for (int i = 0; i < n; i++) {
 		    for (int j = 1; j < (n - i-1); j++) {
 
@@ -30,10 +31,13 @@ public class BubbleSortRandom {
 		            list[j - 1] = list[j];
 		            list[j] = temp;
 		        }
+		        count++;
 		    }
 		}
 		long ems = System.currentTimeMillis();
 		long ms = ems - sms;
+		System.out.println("number of operations = " + count);
 		System.out.println( "Time take = "+ms);
+		System.out.println("time taken by each operations = " + count/ms);
 	}
 }
